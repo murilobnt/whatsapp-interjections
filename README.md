a548d177a3fc3b4ba9e08f4dc6e60a59bd468f3f
murilobnt <murilo.araujo.md@gmail.com>
2020-11-06 23:47:22 -0300

    :books: Adds readme and license

---
+++ README.md
@@ -0,0 +1,26 @@
+## WhatsApp Interjections
+
+This presents a project that uses data from a WhatApp group chat in order to
+visualize how many interjections the users has sent.
+
+This project is written in Python, and it uses the Matplotlib, Numpy, emoji,
+regex, Plotly, and Pandas libraries.
+
+## Output
+
+![output](https://github.com/murilobnt/whatsapp-interjections/blob/master/output/wpp-analysis.png?raw=true)
+
+## How to run
+
+Initial requirements:
+* Python 3
+* Pip
+
+```sh
+python3 -m pip install -r requirements.txt
+python3 yta.py
+```
+
+### License
+
+WhatsApp Interjections is licensed under the [MIT License](https://github.com/murilobnt/bc19ev/blob/master/LICENSE).
--- wpp_analysis.py
+++ wpp_analysis.py
@@ -15,7 +15,7 @@ def starts_with_date_time(s):
         return True
     return False
 
-def find_author(s):
+def has_author(s):
   s=s.split(":")
   if len(s) > 1:
     return True
@@ -30,12 +30,12 @@ def split_count(text):
             emoji_list.append(word)
     return emoji_list
 
-def getDataPoint(line):
-    splitLine = line.split(' - ')
-    dateTime = splitLine[0]
-    date, time = dateTime.split(' ')
-    message = ' '.join(splitLine[1:])
-    if find_author(message):
+def get_data_point(line):
+    split_l = line.split(' - ')
+    date_time = split_l[0]
+    date, time = date_time.split(' ')
+    message = ' '.join(split_l[1:])
+    if has_author(message):
         split_msg = message.split(': ')
         author = split_msg[0]
         message = ' '.join(split_msg[1:])
@@ -58,7 +58,7 @@ with open(path, encoding="utf-8") as fp:
             if len(msg_buffer) > 0:
                 parsed.append([date, time, author, ' '.join(msg_buffer)])
             msg_buffer.clear()
-            date, time, author, message = getDataPoint(line)
+            date, time, author, message = get_data_point(line)
             msg_buffer.append(message)
         else:
             msg_buffer.append(line)
@@ -73,15 +73,15 @@ authors = anon.author.unique()
 anon.author = anon.author.apply(lambda author: 'Author ' + str(np.where(authors == author)[0][0] + 1))
 authors = anon.author.unique()
 
-media_messages = anon[anon['message'] == '<Arquivo de mídia oculto>']
-chat_messages = anon.drop(media_messages.index)
-chat_messages['word_count'] = chat_messages['message'].apply(lambda s : len(s.split(' ')))
-chat_messages["emoji"] = anon["message"].apply(split_count)
-chat_messages['urlcount'] = chat_messages.message.apply(lambda x: re.findall(r'(https?://\S+)', x)).str.len()
-chat_messages['greetings'] = anon.message.apply(lambda msg: True if re.match('([B|b]om dia)|([B|b]oa tarde)|([B|b]oa noite)', msg) else False)
+media_msgs = anon[anon['message'] == '<Arquivo de mídia oculto>']
+text_msgs = anon.drop(media_msgs.index)
+text_msgs['word_count'] = text_msgs['message'].apply(lambda s : len(s.split(' ')))
+text_msgs["emoji"] = anon["message"].apply(split_count)
+text_msgs['urlcount'] = text_msgs.message.apply(lambda x: re.findall(r'(https?://\S+)', x)).str.len()
+text_msgs['greetings'] = anon.message.apply(lambda msg: True if re.match('([B|b]om dia)|([B|b]oa tarde)|([B|b]oa noite)', msg) else False)
 
-auth = chat_messages.groupby("author").sum()
-auth.reset_index(inplace=True)
-fig = px.bar(auth, y="author", x="greetings", color="author", color_discrete_sequence=["red", "green", "blue", "goldenrod", "magenta"])
+author_group = text_msgs.groupby("author").sum()
+author_group.reset_index(inplace=True)
+fig = px.bar(author_group, y="author", x="greetings", color="author", color_discrete_sequence=["red", "green", "blue", "goldenrod", "magenta"])
 
 plotly.offline.plot(fig, filename='output/wpp_analysis.html')
